9f762f8711b7265d3be7a684b43a8533bf9ad059
AmnaShahid987 <amna.shahid123@gmail.com>
2026-01-29 13:02:18 +0500

    Update train.py

--- train.py
+++ train.py
@@ -73,11 +73,11 @@ def squash(x, midpoint=0.75, steepness=6):
 employment_map = {'Salaried': 10, 'Pensioner': 5, 'Self-Employed': 7}
 
 base_score = (
-    0.20 * df['age'].apply(age_score) +
+    0.30 * df['age'].apply(age_score) +
     0.30 * df['employment_status'].map(employment_map).fillna(0.5) +
     0.20 * df['household_dependents'].apply(dependent_score) +
     0.10 * df['marital_status'].map({'Married': 10, 'Single': 8}).fillna(0.8) +
-    0.20 * df['city'].apply(city_score)
+    0.10 * df['city'].apply(city_score)
 )
 
 df['life_stability_score'] = (base_score - df.apply(instability_penalty, axis=1)).clip(0, 1)
